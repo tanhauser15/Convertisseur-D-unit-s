196f3b84313f2df12957072add7c2170404023dd
Josias KESSALE <jtkessale@gmail.com>
2025-09-19 16:37:54 +0100

    Ajout des fonctions de bouclages sur les convertions dans le fichier variable.py

--- variable.py
+++ variable.py
@@ -1,10 +1,34 @@
 
 def convetionCmPouces():
-    valuer_a_convertir = float(input("Entrez la valeur à convertir: "))
+    valuer_a_convertir = float(input("Entrez la valeur à convertir en pouce: "))
     ResultatConvertion = valuer_a_convertir*0.394
     print(f"{valuer_a_convertir} cm = {ResultatConvertion} pouces")
+    
+    choixContinuer = int(input("Continuer la conversion ? 1 'pour oui' ou 0 'pour non'"))
+    if choixContinuer == 1:
+        bouclerConversionCmPouces(choixContinuer)
 
 def convetionPoucesCm():
-    valuer_a_convertir = float(input("Entrez la valeur à convertir: "))
+    valuer_a_convertir = float(input("Entrez la valeur à convertir en cm: "))
     ResultatConvertion = valuer_a_convertir*2.54
     print(f"{valuer_a_convertir} pouces = {ResultatConvertion} cm")
+
+    choixContinuer = int(input("Continuer la conversion ? 1 'pour oui' ou 0 'pour non'"))
+    if choixContinuer == 1:
+        bouclerConversionPoucesCm(choixContinuer)
+
+def bouclerConversionPoucesCm(choix):
+    while choix==1:
+        valuer_a_convertir = float(input("Entrez la valeur à convertir en cm: "))
+        ResultatConvertion = valuer_a_convertir*2.54
+        print(f"{valuer_a_convertir} pouces = {ResultatConvertion} cm")
+        
+        choix = int(input("Continuer la conversion ? 1 'pour oui' ou 0 'pour non'"))
+
+def bouclerConversionCmPouces(choix):
+    while choix==1:
+        valuer_a_convertir = float(input("Entrez la valeur à convertir en pouce: "))
+        ResultatConvertion = valuer_a_convertir*0.394
+        print(f"{valuer_a_convertir} cm = {ResultatConvertion} pouces")
+        
+        choix = int(input("Continuer la conversion ? 1 'pour oui' ou 0 'pour non'"))
