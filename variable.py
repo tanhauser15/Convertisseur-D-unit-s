03bd2233e06ee9dcd92726399da2d0c7924503cd
Josias KESSALE <jtkessale@gmail.com>
2025-09-19 16:15:58 +0100

    Implementation des deux fonction de conversion et du fichier main

---
+++ variable.py
@@ -0,0 +1,10 @@
+
+def convetionCmPouces():
+    valuer_a_convertir = float(input("Entrez la valeur à convertir: "))
+    ResultatConvertion = valuer_a_convertir*0.394
+    print(f"{valuer_a_convertir} cm = {ResultatConvertion} pouces")
+
+def convetionPoucesCm():
+    valuer_a_convertir = float(input("Entrez la valeur à convertir: "))
+    ResultatConvertion = valuer_a_convertir*2.54
+    print(f"{valuer_a_convertir} pouces = {ResultatConvertion} cm")
