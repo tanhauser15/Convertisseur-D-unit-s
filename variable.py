3e2d97f9ba9aee2e7b2183733f256f60e23b7427
Josias KESSALE <jtkessale@gmail.com>
2025-09-20 09:27:51 +0100

    Ajout de la fonction de bouclage sur le menu principal et de sont appel dans le fichier principal main

--- variable.py
+++ variable.py
@@ -32,3 +32,29 @@ def bouclerConversionCmPouces(choix):
         print(f"{valuer_a_convertir} cm = {ResultatConvertion} pouces")
         
         choix = int(input("Continuer la conversion ? 1 'pour oui' ou 0 'pour non'"))
+
+def bouclerMenuPrincipale(continuer):
+    while continuer:
+            try:
+                choix = int(input("""
+            Souhaitez convertir des "Pouces vers cm" ou "cm vers Pouces"?
+            Tapez:
+            1 - Pour convertir des pouces vers cm
+            2 - Pour convertir des cm vers pouces   
+
+            Entrez votre choix:     
+            """))
+                match choix:
+                    case 1: 
+                        convetionPoucesCm()
+                    case 2:
+                        convetionCmPouces()
+                    case _:
+                        print("Vous avez saisi la mauvais choix veillez réessayer.")
+            except ValueError:
+                print("Seuls les chiffres sont autorisés. Veuillez réessayer.")
+            
+            choixContinuer = int(input("Retour au menu principal: 1 'oui' 0 'non': "))
+            if choixContinuer == 0:
+                continuer = False
+                print("Fin du programme")
